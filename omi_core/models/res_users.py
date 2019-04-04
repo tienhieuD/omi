8b2203cb33ae029067d1f1aefeb630af3fa27a43
tienhieud <tienhieud@github.com>
2019-04-04 12:47:18 +0700

    Omi core add image user

--- omi_core/models/res_users.py
+++ omi_core/models/res_users.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
+import base64
+import requests
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     psid = fields.Char("Facebook Page Sender ID")
     page_id = fields.Char("Facebook Page ID")
+
+    def get_as_base64(self, url):
+        return base64.b64encode(requests.get(url).content)
 
     @api.model
     def get_create_partner_from_psid(self, psid, page_id):
@@ -20,8 +25,7 @@ class ResPartner(models.Model):
         FacebookUtils = self.env['omi.facebook.utils']
         user_info = FacebookUtils.get_sender_info(page_id=page_id, sender_id=psid)
         user_name = user_info['first_name'] + ' ' + user_info['last_name']
-        # TODO: Làm hình người dùng sau
-        # image = user_info['profile_pic']
-        partner = self.create({'name': user_name, 'psid': psid, 'page_id': page_id,})
+        image = self.get_as_base64(user_info['profile_pic'])
+        partner = self.create({'name': user_name, 'psid': psid, 'page_id': page_id, 'image': image})
         return partner
 
